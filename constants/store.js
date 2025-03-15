bf04ba9ad1f3b8569e383b2995a89eaca1d9917f
tymyevhenmpw <tymyevhen@gmail.com>
2025-03-15 23:54:47 +0100

    Initial commit

---
+++ constants/store.js
@@ -0,0 +1,12 @@
+export const Store = {
+  "name": "Tech 3.0",
+  "currency": "UAH",
+  "currency_sign": "₴",
+  "domain": "https://fo-scandinavia.vercel.app",
+  "database": "Tech30_2025_03_15",
+  "default_image": {
+    "product_card": 0,
+    "product_page": 0,
+    "checkout_page": 0
+  }
+};
